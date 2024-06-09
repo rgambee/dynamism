072e4abf42b611a157a397f1e32fbab2bbc58cd1
Robert Gambee <robert@formlabs.com>
2024-06-09 12:35:35 -0400

    Animate oscillating circle

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
-export default function Home() {
+'use client';
+
+import { Canvas } from '@react-three/fiber';
+
+import OscillatingCircle from './systems/SimpleHarmonicOscillator.tsx';
+
+function MyCanvas() {
   return (
-    <main>
-    </main>
+    <Canvas orthographic >
+      <ambientLight intensity={0.1} />
+      <OscillatingCircle />
+    </Canvas>
   );
 }
+
+function App() {
+  return <MyCanvas />;
+}
+
+export default App;
---
+++ app/systems/SimpleHarmonicOscillator.tsx
@@ -0,0 +1,46 @@
+import { useFrame } from '@react-three/fiber';
+import { Ref, useRef } from 'react';
+import { Mesh } from 'three';
+
+
+function animate(
+  meshRef: Ref<Mesh>,
+  initialVelocity: number,
+  springConstant: number,
+) {
+  let velX = initialVelocity;
+
+  return (_, timeStep: number) => {
+    const mesh = meshRef.current as Mesh | null | undefined;
+    if (!mesh) {
+      return;
+    }
+    velX -= mesh.position.x * springConstant * timeStep;
+    mesh.position.x += velX * timeStep;
+  };
+}
+
+interface OscillatingCircleParams {
+  circleRadius: number;
+  initialVelocity: number;
+  springConstant: number;
+}
+
+function OscillatingCircle({
+  circleRadius = 10.0,
+  initialVelocity = 100.0,
+  springConstant = 1.0,
+} : OscillatingCircleParams) {
+  const meshRef = useRef<Mesh>();
+
+  useFrame(animate(meshRef, initialVelocity, springConstant));
+
+  return (
+    <mesh ref={meshRef} position={[0, 0, 0]}>
+      <circleGeometry args={[circleRadius]} />
+      <meshBasicMaterial color="royalblue" />
+    </mesh>
+  );
+}
+
+export default OscillatingCircle;
