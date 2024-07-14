1a4d153a46519fea70d07808c5a1a2a1b57c442d
Robert Gambee <robert@formlabs.com>
2024-07-14 15:18:33 -0400

    Animate Lissajous curves

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,15 @@
 'use client';
 
 import { Canvas } from '@react-three/fiber';
-import { Vector3 } from 'three';
 
-import GravitationalSystem from './systems/GravitationalSystem.tsx';
+import LissajousArray from './systems/LissajousArray';
 
 function MyCanvas() {
-  const positions = [
-    new Vector3(-50, 0, 0),
-    new Vector3(50, 0, 0),
-    new Vector3(0, 100, 0),
-  ];
 
   return (
     <Canvas orthographic >
       <ambientLight intensity={0.1} />
-      {/*<OscillatingCircle />*/}
-      <GravitationalSystem
-        initialPositions={positions}
-      />
+      <LissajousArray />
     </Canvas>
   );
 }
